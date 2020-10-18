929369c3eb904fd36a58649ee5f21cc6bb7de501
ShuvoHassan <shuvo.hassan@hotmail.com>
2020-10-18 14:43:23 +0600

    MUI styling part 1 done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,21 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import Checkbox from '@material-ui/core/Checkbox'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import TextField from '@material-ui/core/TextField'
-import { makeStyles } from '@material-ui/core/styles'
+import {
+    makeStyles,
+    ThemeProvider,
+    createMuiTheme,
+} from '@material-ui/core/styles'
+import { green, orange, blue, pink } from '@material-ui/core/colors'
+import Typography from '@material-ui/core/Typography'
+import 'fontsource-roboto'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const useStyles = makeStyles({
     root: {
@@ -19,6 +33,22 @@ const useStyles = makeStyles({
         color: 'white',
         fontSize: 32,
         padding: '5px 30px',
+    },
+})
+
+const theme = createMuiTheme({
+    typography: {
+        h4: {
+            margin: 15,
+        },
+    },
+    palette: {
+        primary: {
+            main: blue[400],
+        },
+        secondary: {
+            main: pink[400],
+        },
     },
 })
 
@@ -50,65 +80,119 @@ function CheckboxExample() {
 
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <h3>Textfields</h3>
-                <TextField
-                    variant='filled'
-                    color='secondary'
-                    type='time'
-                    label='The time'
-                />
-                <TextField
-                    variant='filled'
-                    color='secondary'
-                    type='email'
-                    label='Type your email'
-                    placeholder='test@test.com'
-                />
-                <h3>Cutom Checkbox</h3>
-                <CheckboxExample />
-                <h3>Button Styles</h3>
-                <CustomStyledButton />
-                <ButtonGroup>
-                    <Button
-                        startIcon={
-                            <SaveIcon
+        <ThemeProvider theme={theme}>
+            <Container maxWidth='lg'>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <IconButton
+                            edge='start'
+                            color='inherit'
+                            aria-label='menu'
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant='h6' style={{ flexGrow: 1 }}>
+                            MUI Themeing
+                        </Typography>
+                        <Button color='inherit'>Login</Button>
+                    </Toolbar>
+                </AppBar>
+                <div className='App'>
+                    <header className='App-header'>
+                        <Typography variant='h2' component='div'>
+                            Welcome to MUI
+                        </Typography>
+                        <Typography variant='h4'>Textfields</Typography>
+                        <Grid container spacing={4} justify='center'>
+                            <Grid item>
+                                <TextField
+                                    variant='filled'
+                                    color='secondary'
+                                    type='time'
+                                    label='The time'
+                                />
+                            </Grid>
+                            <Grid item>
+                                <TextField
+                                    variant='filled'
+                                    color='secondary'
+                                    type='email'
+                                    label='Type your email'
+                                    placeholder='test@test.com'
+                                />
+                            </Grid>
+                        </Grid>
+                        <Typography variant='h4'>Cutom Checkbox</Typography>
+                        <CheckboxExample />
+                        <Typography variant='h4'>Responsive Grid</Typography>
+                        <Grid
+                            container
+                            spacing={2}
+                            justify='center'
+                            style={{
+                                padding: 10,
+                            }}
+                        >
+                            <Grid item xs={4} sm={4} md={6} lg={12}>
+                                <Paper
+                                    style={{ width: '100%', height: 100 }}
+                                ></Paper>
+                            </Grid>
+                            <Grid item xs={4} sm={4} md={6} lg={12}>
+                                <Paper
+                                    style={{ width: '100%', height: 100 }}
+                                ></Paper>
+                            </Grid>
+                            <Grid item xs={4} sm={4} md={6} lg={12}>
+                                <Paper
+                                    style={{ width: '100%', height: 100 }}
+                                ></Paper>
+                            </Grid>
+                        </Grid>
+
+                        <Typography variant='h4'>Button Styles</Typography>
+                        <CustomStyledButton />
+                        <ButtonGroup>
+                            <Button
+                                startIcon={
+                                    <SaveIcon
+                                        style={{
+                                            fontSize: 32,
+                                        }}
+                                    />
+                                }
+                                variant='contained'
+                                color='primary'
+                                size='large'
                                 style={{
                                     fontSize: 32,
                                 }}
-                            />
-                        }
-                        variant='contained'
-                        color='primary'
-                        size='large'
-                        style={{
-                            fontSize: 32,
-                        }}
-                    >
-                        Save
-                    </Button>
-                    <Button
-                        startIcon={
-                            <DeleteIcon
+                            >
+                                Save
+                            </Button>
+                            <Button
+                                startIcon={
+                                    <DeleteIcon
+                                        style={{
+                                            fontSize: 32,
+                                        }}
+                                    />
+                                }
+                                variant='contained'
+                                color='secondary'
+                                size='large'
                                 style={{
                                     fontSize: 32,
                                 }}
-                            />
-                        }
-                        variant='contained'
-                        color='secondary'
-                        size='large'
-                        style={{
-                            fontSize: 32,
-                        }}
-                    >
-                        Discard
-                    </Button>
-                </ButtonGroup>
-                <img src={logo} className='App-logo' alt='logo' />
-            </header>
-        </div>
+                            >
+                                Discard
+                            </Button>
+                        </ButtonGroup>
+                        <img src={logo} className='App-logo' alt='logo' />
+                    </header>
+                </div>
+            </Container>
+        </ThemeProvider>
     )
 }
 
