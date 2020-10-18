4d114fb98a91ddc775a22aa101e7b935c859042b
ShuvoHassan <shuvo.hassan@hotmail.com>
2020-10-18 13:04:17 +0600

    Custom button, checkbox, textfield

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+        border: 0,
+        marginBottom: 15,
+        borderRadius: 30,
+        color: 'white',
+        fontSize: 32,
+        padding: '5px 30px',
+    },
+})
+
+function CustomStyledButton() {
+    const classes = useStyles()
+    return <Button className={classes.root}>Custom Styled Button</Button>
 }
 
-export default App;
+function CheckboxExample() {
+    const [checked, setChecked] = React.useState(false)
+    return (
+        <FormControlLabel
+            control={
+                <Checkbox
+                    checked={checked}
+                    onChange={(e) => setChecked(e.target.checked)}
+                    icon={<DeleteIcon />}
+                    checkedIcon={<SaveIcon />}
+                    color='secondary'
+                    inputProps={{
+                        'aria-label': 'secondary checkbox',
+                    }}
+                />
+            }
+            label='Testing Checkbox'
+        />
+    )
+}
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h3>Textfields</h3>
+                <TextField
+                    variant='filled'
+                    color='secondary'
+                    type='time'
+                    label='The time'
+                />
+                <TextField
+                    variant='filled'
+                    color='secondary'
+                    type='email'
+                    label='Type your email'
+                    placeholder='test@test.com'
+                />
+                <h3>Cutom Checkbox</h3>
+                <CheckboxExample />
+                <h3>Button Styles</h3>
+                <CustomStyledButton />
+                <ButtonGroup>
+                    <Button
+                        startIcon={
+                            <SaveIcon
+                                style={{
+                                    fontSize: 32,
+                                }}
+                            />
+                        }
+                        variant='contained'
+                        color='primary'
+                        size='large'
+                        style={{
+                            fontSize: 32,
+                        }}
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        startIcon={
+                            <DeleteIcon
+                                style={{
+                                    fontSize: 32,
+                                }}
+                            />
+                        }
+                        variant='contained'
+                        color='secondary'
+                        size='large'
+                        style={{
+                            fontSize: 32,
+                        }}
+                    >
+                        Discard
+                    </Button>
+                </ButtonGroup>
+                <img src={logo} className='App-logo' alt='logo' />
+            </header>
+        </div>
+    )
+}
+
+export default App
